da6905533bd9f890cf6a6d8ea55469e00770c1b5
peekxc <matt.piekenbrock@gmail.com>
2023-12-11 12:58:36 -0500

    Update main workflows; need to refactor wheel.yml to use tools to incorporate openmp support

--- src/skpackage/__init__.py
+++ src/skpackage/__init__.py
@@ -1,25 +1,27 @@
-__version__ = '0.0.1'
-from _prefixsum import prefixsum
+import importlib.metadata
+__version__ = importlib.metadata.version("primate")
+
+import _prefixsum # import extension module
 
 ## Based on Numpy's usage: https://github.com/numpy/numpy/blob/v1.25.0/numpy/lib/utils.py#L75-L101
 def get_include():
   """Return the directory that contains the packages \\*.h header files.
 
-  Extension modules that need to compile against primate should use this
+  Extension modules that need to compile against the packages exported header files should use this
   function to locate the appropriate include directory.
 
   Notes: 
     When using `distutils`, for example in `setup.py`:
       ```python
-      import my_package as mypkg
+      import skpackage as sk
       ...
-      Extension('extension_name', ..., include_dirs=[mypkg.get_include()])
+      Extension('extension_name', ..., include_dirs=[sk.get_include()])
       ...
       ```
     Or with `meson-python`, for example in `meson.build`:
       ```meson
       ...
-      run_command(py, ['-c', 'import my_package as mypkg; print(mypkg.get_include())', check : true).stdout().strip()
+      run_command(py, ['-c', 'import skpackage as sk; print(sk.get_include())', check : true).stdout().strip()
       ...
       ```
   """

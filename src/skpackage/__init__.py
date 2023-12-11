4b9b96aee972a4cdefa53240bff852b18f5b7d3f
peekxc <matt.piekenbrock@gmail.com>
2023-12-11 13:36:23 -0500

    update init

--- src/skpackage/__init__.py
+++ src/skpackage/__init__.py
@@ -1,5 +1,5 @@
 import importlib.metadata
-__version__ = importlib.metadata.version("primate")
+__version__ = importlib.metadata.version("skpackage")
 
 import _prefixsum # import extension module
 
